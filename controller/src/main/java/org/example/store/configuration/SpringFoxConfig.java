811668d3d731792b30b9eaf32b93e48505079d58
Yuriy Lukashevich <yuriy_lukashevich@epam.com>
2020-08-14 01:52:22 +0300

    feat: : add dependency for swagger to show validation limits

--- controller/src/main/java/org/example/store/configuration/SpringFoxConfig.java
+++ controller/src/main/java/org/example/store/configuration/SpringFoxConfig.java
@@ -2,6 +2,8 @@ package org.example.store.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,12 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
           .select()                                  
-          .apis(RequestHandlerSelectors.any())
+          .apis(RequestHandlerSelectors.basePackage("org.example.store.controller"))
           .paths(PathSelectors.any())
           .build();                                           
     }
